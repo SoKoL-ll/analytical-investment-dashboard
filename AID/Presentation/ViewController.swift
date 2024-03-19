d7b21da9945dd434293f820ed01f5beb4c2b83d7
SoKoL-ll <hujikolp1907>
2024-03-19 23:09:50 +0300

    Improved project structure and added snapkit

--- AID/Presentation/ViewController.swift
+++ AID/Presentation/ViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
-
-
 }
 
