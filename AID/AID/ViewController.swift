85448dc43f575155bb0eb1351fe36ce301c903b8
SoKoL-ll <hujikolp1907>
2024-03-19 17:35:00 +0300

    empty project

---
+++ AID/AID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AID
+//
+//  Created by Alexandr Sokolov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
