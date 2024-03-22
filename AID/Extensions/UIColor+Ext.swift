c9fdbf7f055d651c62322f1a629ef38fb3bab45a
fix1in1it <fix1in1it@gmail.com>
2024-03-22 14:48:34 +0300

    added palette+extensions

---
+++ AID/Extensions/UIColor+Ext.swift
@@ -0,0 +1,40 @@
+//
+//  UIColor+Ext.swift
+//  AID
+//
+//  Created by Alexander on 22.03.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background: UIColor = {
+        guard let color = UIColor(named: "Background") else {
+            fatalError("Can't find color: Background")
+        }
+        
+        return color
+    }()
+    
+    static let accent: UIColor = {
+        guard let color = UIColor(named: "Thistle") else {
+            fatalError("Can't find color: Thistle")
+        }
+        
+        return color
+    }()
+    
+    static let positive: UIColor = {
+        guard let color = UIColor(named: "Green") else {
+            fatalError("Can't find color: Green")
+        }
+        return color
+    }()
+    
+    static let negative: UIColor = {
+        guard let color = UIColor(named: "Red") else {
+            fatalError("Can't find color: Red")
+        }
+        return color
+    }()
+}
