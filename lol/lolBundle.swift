ca0212b6cd50e818f9009890dbe44985e523f911
Alexander <fix1in1it@gmail.com>
2024-03-29 00:57:00 +0300

    proj yml refactoring

---
+++ lol/lolBundle.swift
@@ -0,0 +1,16 @@
+//
+//  lolBundle.swift
+//  lol
+//
+//  Created by Alexander on 29.03.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct lolBundle: WidgetBundle {
+    var body: some Widget {
+        lol()
+    }
+}
